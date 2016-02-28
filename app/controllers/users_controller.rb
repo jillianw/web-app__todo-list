a32c62e2f368ddd0913852cf933ef5924bef0412
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-28 12:05:30 -0600

    changed naming conventions for users views/controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,55 @@
 # This controller is for all the CRUD operations related to a User.
 
 # Shows the form for adding a user
-MyApp.get "/new_user" do 
-  erb :"users/add"
+MyApp.get "/users/new" do 
+  erb :"users/new"
 end
 
 # Processes the form for adding a user
-MyApp.post "/user_added" do 
+MyApp.post "/users/create" do 
   @user = User.new
   @user.name = params["name"]
   @user.email = params["email"]
   @user.password = params["password"]
   if @user.is_valid == true
     @user.save
-    redirect "/view_user/#{@user.id}"
+    redirect "/users/#{@user.id}"
   else
     # return error message
-    erb :"users/add"
+    erb :"users/new"
   end
 end
 
 # Shows all users added
-MyApp.get "/all_users" do 
+MyApp.get "/users" do 
   @current_user = User.find_by_id(session["user_id"])
   if session["user_id"] != nil
-    @all_users = User.all 
-    erb :"users/view_all"
+    @users = User.all 
+    erb :"users/index"
   else
     erb :"please_login"
   end
 end
 
 # Shows one user
-MyApp.get "/view_user/:num" do 
+MyApp.get "/users/:id" do 
   @current_user = User.find_by_id(session["user_id"])
   if session["user_id"] != nil
-    @one_user = User.find_by_id(params[:num])
-    erb :"users/view_one" 
+    @user = User.find_by_id(params[:id])
+    erb :"users/show" 
   else
     erb :"please_login"
   end
 end
 
-# Processes deletion of a user (from view_all page)
-MyApp.post "/delete_user/:num" do
+# Processes deletion of a user (from index page)
+MyApp.post "/users/:id/delete" do
   @current_user = User.find_by_id(session["user_id"])
   if session["user_id"] != nil
-    @delete_user = User.find_by_id(params[:num]) 
-    @this_user_todo_lists = Todo.where({"user_id" => params[:num]})
+    @user = User.find_by_id(params[:id]) 
+    @this_user_todo_lists = Todo.where({"user_id" => params[:id]})
     @this_user_todo_lists.delete_all
-    @delete_user.delete
+    @user.delete
     erb :"users/delete"
   else
     erb :"please_login"
@@ -57,30 +57,30 @@ MyApp.post "/delete_user/:num" do
 end
 
 #Shows form for updating a user
-MyApp.get "/update_user/:num" do 
+MyApp.get "/users/:id/edit" do 
   @current_user = User.find_by_id(session["user_id"])
   if session["user_id"] != nil
-    @user_update = User.find_by_id(params[:num]) 
-    erb :"users/update"
+    @user = User.find_by_id(params[:id]) 
+    erb :"users/edit"
   else
     erb :"please_login"
   end  
 end
 
 #Processes the form for updating a user
-MyApp.post "/updated_user/:num" do 
+MyApp.post "/users/:id/update" do 
   @current_user = User.find_by_id(session["user_id"])
   if session["user_id"] != nil
-    @update_user = User.find_by_id(params[:num])
-    @update_user.name = params["name"]
-    @update_user.email = params["email"]
-    @update_user.password = params["password"]
+    @user = User.find_by_id(params[:id])
+    @user.name = params["name"]
+    @user.email = params["email"]
+    @user.password = params["password"]
   else
     erb :"please_login"
   end  
 
-  if @update_user.is_valid == true
-    @update_user.save
+  if @user.is_valid == true
+    @user.save
     erb :"users/updated"
   else
     erb :"users/update_error"
